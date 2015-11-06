a9668303848ea6ba11e410a6a4d54b94d7d6a10c
perlancar (@pc-office) <perlancar@gmail.com>
2015-11-06 19:05:04 +0700

    Hackety hack (unfinished)

--- lib/App/VerifyBackup.pm
+++ lib/App/VerifyBackup.pm
@@ -6,8 +6,20 @@ package App::VerifyBackup;
 use 5.010001;
 use strict;
 use warnings;
+use Log::Any::IfLOG '$log';
+
+use File::chdir;
 
 our %SPEC;
+
+my $sqlspec = {
+    latest_v => 1,
+    install => [
+        # files in the root of dir will have
+        'CREATE TABLE IF NOT EXISTS file (name VARCHAR(255), pid INT NOT NULL, is_dir INT NOT NULL DEFAULT 0, size INT NOT NULL, hash_type VARCHAR(8) NOT NULL, hash TEXT)',
+        'CREATE UNIQUE INDEX ix_file__pid__name ON file(pid, name)',
+    ],
+};
 
 $SPEC{':package'} = {
     v => 1.1,
@@ -40,7 +52,7 @@ To use this utility, first, you create the checksum database:
 
     % verify-backup --update-db dir.db dir
 
-Then later and regularly, you verify the backup files in `dir`:
+Then later and regularly, you verify the backup files in `dir` using:
 
     % verify-backup dir.db dir
 
@@ -66,35 +78,70 @@ _
             req => 1,
             pos => 0,
         },
-        files => {
-            schema => ['array*', of=>'str*', min_len=>1],
-            'x.element_completion' => ['filename'],
+        dir => {
+            schema => 'str*',
+            'x.completion' => ['dirname'],
             req => 1,
             pos => 1,
-            greedy => 1,
         },
     },
 };
 sub verify_backup {
     require DBI;
+    require SQL::Schema::Versioned;
 
     my %args = @_;
 
-    my $action = $args{action};
+    my $action = $args{action} // 'verify';
     my $db = $args{db};
-    my $files = $args{files};
+    my $dir = $args{dir};
 
     if ($action eq 'verify') {
         return [412, "Checksum database must already exist"] unless -f $db;
     }
 
     my $dbh = DBI->connect("dbi:SQLite:dbname=$db", "", "");
+    my $res = SQL::Schema::Versioned::create_or_update_db_schema(
+        dbh=>$dbh, spec=>$sqlspec);
+    return $res unless $res->[0] == 200;
+
+    local $CWD = $dir;
+
+    my $code_find = sub {
+        my ($prefix, $pid) = @_;
+        opendir my($dh), "." or do {
+            $log->warnf("Can't chdir into %s, skipped", $prefix);
+            return;
+        };
+        while (defined(my $e = readdir($dh))) {
+            next if $e eq '.' || $e eq '..';
+            $log->tracef("Processing %s/%s", $prefix, $e);
+
+            # if symlink, stat the symlink itself
+            my @st = stat($e);
+            my @rst;
+            if (-l _) {
+                @rst = @st;
+                @st = lstat($e);
+            }
+
+            my @rst;
+            if (-l _) {
+
+            my @st = lstat($e);
+
+        }
+    };
+
+    $code_find->(".", 0);
 
     if ($action eq 'update_db') {
         # XXX
     } elsif ($action eq 'verify') {
         # XXX
     }
+
+    [200];
 }
 
 1;
